4af94711896b94cd2780f969950d584a24a870d6
Maxpowersi <maxi1990@gmail.com>
2020-02-01 03:28:59 -0300

    adapted for EvilPortalReloaded

---
+++ portals/default/success.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Now you are connected to internet</title>
+    <meta name="title" content="Telecentro WiFi">
+    <meta name="description" content="Conectate gratis en la calle - Accedé a la nueva red WiFi **Sólo clientes**">
+
+    <meta name="mobile-web-app-capable" content="yes">
+</head>
+
+<body style="background-color:#5BA6BF; font-family: 'proxima-nova-soft', 'Proxima Nova Soft', Helvetica, Arial, sans-serif; font-size: 2rem; color:#3f3844; font-weight: 700; letter-spacing:0.025em;">
+    <div>
+        <header></header>
+        <div>
+            <main>
+                <div >
+                    <center>Now you are connected to internet</center>
+                </div>
+            </main>
+
+        </div>
+        <footer></footer>
+    </div>
+
+</body>
+
+</html>
